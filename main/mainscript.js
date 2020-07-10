47ec8b924baa5e4a8461319c6b89f9573603e780
gridrising <65217494+gridrising@users.noreply.github.com>
2020-07-10 21:45:35 +0300

    need to male filter fo toDO

--- main/mainscript.js
+++ main/mainscript.js
@@ -1,7 +1,7 @@
 const inputAdd = document.querySelector('.add_form');
 const tableWithLi = document.querySelector('.to_do_list-table');
 const tableWithRemoveLi = document.querySelector('.to_do_list-table')
-
+const filterForm = document.querySelector('#filter_form-radio')
 inputAdd.addEventListener("submit", (event) => {
 
     event.preventDefault();
@@ -23,10 +23,61 @@ inputAdd.addEventListener("submit", (event) => {
 
 tableWithRemoveLi.addEventListener('click', (event) => {
     if (event.target.closest(".remove-button")) {
-        const liWillRemoved = event.target.closest('.to_do_list-One');
+        let liWillRemoved;
+        if (event.target.closest('.to_do_list-One')){
+            liWillRemoved = event.target.closest('.to_do_list-One');
+        }
+        else{
+            liWillRemoved = event.target.closest('.to_do_list-One-completed');
+        }
         const buttonWillRemoved = event.target;
 
         liWillRemoved.remove();
         buttonWillRemoved.remove();
     }
 })
+
+tableWithLi.addEventListener('click',(event) => {
+    if (event.target.closest('.to_do_list-One')){
+        event.target.className = 'to_do_list-One-completed';
+    }
+})
+
+filterForm.addEventListener('click',(event) => {
+    if(event.target.closest('#sorted-by-all')){
+        for (let oneLi of document.getElementsByTagName('li')){
+           console.log(oneLi);
+           console.log(oneLi.style)
+            if (oneLi.style.display === 'hidden'){
+                oneLi.style.display = '';
+            }
+        }
+    }
+    else if (event.target.closest('#sorted-by-completed')){
+        for (let oneLi of document.getElementsByTagName('li')){
+            console.log(oneLi);
+            console.log(oneLi.style)
+            if(oneLi.className === '.to_do_list-One-completed'){
+                oneLi.style.display = '';
+            }
+            else{
+                oneLi.style.display = 'hidden';
+            }
+        }
+
+    }
+    else if (event.target.closest('#sorted-by-uncompleted')){
+        for (let oneLi of document.getElementsByTagName('li')){
+            console.log(oneLi);
+            console.log(oneLi.style)
+            if(oneLi.className === '.to_do_list-One'){
+                oneLi.style.display = '';
+            }
+            else{
+                oneLi.style.display = 'hidden';
+            }
+        }
+
+    }
+})
+
