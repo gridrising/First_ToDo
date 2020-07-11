e21c1073b8d7035f6242ebb8bff159bfc14329da
gridrising <65217494+gridrising@users.noreply.github.com>
2020-07-11 15:40:34 +0300

    completed

--- main/mainscript.js
+++ main/mainscript.js
@@ -3,6 +3,11 @@ const tableWithLi = document.querySelector('.to_do_list-table');
 const tableWithRemoveLi = document.querySelector('.to_do_list-table')
 const filterForm = document.querySelector('#filter_form-radio')
 const searchForm = document.querySelector('.search_form')
+const searchFormInput = document.querySelector('.search_form-input')
+const sortedByAll = document.querySelector('#sorted-by-all')
+const sortedByCompleted = document.querySelector('#sorted-by-completed');
+
+
 inputAdd.addEventListener("submit", (event) => {
 
     event.preventDefault();
@@ -11,6 +16,9 @@ inputAdd.addEventListener("submit", (event) => {
     const li = document.createElement("li");
     li.className = "to_do_list-One";
     li.textContent = inputWithText.value;
+    if (sortedByCompleted.checked){
+        li.style = 'display:none';
+    }
 
     const button = document.createElement("button");
     button.className = "remove-button";
@@ -69,6 +77,7 @@ filterForm.addEventListener('change', (event) => {
         }
 
     }
+    searchFormInput.value = '';
 })
 
 searchForm.addEventListener('input', (event) => {
@@ -81,10 +90,32 @@ searchForm.addEventListener('input', (event) => {
     for (let oneLi of allListOfLi) {
         let textValueOfLi = oneLi.textContent || oneLi.innerText;
         textValueOfLi = textValueOfLi.toLowerCase();
-        if (textValueOfLi.indexOf(filter) > -1) {
-            oneLi.style = "display:";
+        if (sortedByAll.checked) {
+            if (textValueOfLi.indexOf(filter) > -1) {
+                oneLi.style = "display:";
+            } else {
+                oneLi.style = "display:none";
+            }
         } else {
-            oneLi.style = "display:none";
+            if (sortedByCompleted.checked) {
+                console.log(oneLi);
+                if (oneLi.closest('.to_do_list-One-completed')) {
+                    if (textValueOfLi.indexOf(filter) > -1) {
+                        oneLi.style = "display:";
+                    } else {
+                        oneLi.style = "display:none";
+                    }
+                }
+            } else {
+                console.log(oneLi);
+                if (oneLi.closest('.to_do_list-One')) {
+                    if (textValueOfLi.indexOf(filter) > -1) {
+                        oneLi.style = "display:";
+                    } else {
+                        oneLi.style = "display:none";
+                    }
+                }
+            }
         }
     }
 })
