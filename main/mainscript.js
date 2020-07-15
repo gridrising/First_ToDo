940b8de2fe2c0516d4adcb028e9fac7d285c451b
gridrising <65217494+gridrising@users.noreply.github.com>
2020-07-15 14:05:48 +0300

    remaked with arrayOfLi

--- main/mainscript.js
+++ main/mainscript.js
@@ -1,139 +1,121 @@
-const inputAdd = document.querySelector('.add_form');
-const inputWithText = inputAdd.querySelector('#add_form-input');
-const tableWithLi = document.querySelector('.to_do_list-table');
+const addToDoForm = document.querySelector('.add_form');
+const textOfAddToDoForm = addToDoForm.querySelector('#add_form-input');
+const tableWithToDo = document.querySelector('.to_do_list-table');
+const arrayWithToDo = [];
 const filterForm = document.querySelector('.filter_form-radio');
-const searchForm = document.querySelector('.search_form');
+const listOfFilterButtons = filterForm.querySelectorAll('.filter_form-button')
 const searchFormInput = document.querySelector('.search_form-input');
 const sortedByAll = document.querySelector('#sorted-by-all');
 const sortedByCompleted = document.querySelector('#sorted-by-completed');
 const sortedByUncompleted = document.querySelector('#sorted-by-uncompleted');
-let currentButton = sortedByAll;
-const createLi = () => {
-    const li = document.createElement("li");
-    li.classList.add("to_do_list-One");
-    li.textContent = inputWithText.value;
-    if (currentButton == sortedByCompleted) {
-        li.classList.add('to_do_list-One-hidden');
+let filterCurrentButton = sortedByAll;
+const createToDo = () => {
+    const oneToDo = document.createElement("li");
+    oneToDo.classList.add("to_do_list-One");
+    oneToDo.textContent = textOfAddToDoForm.value;
+    if (filterCurrentButton == sortedByCompleted) {
+        oneToDo.classList.add('to_do_list-One-hidden');
     }
-    return li;
+    return oneToDo;
 }
-const createButton = () => {
-    const button = document.createElement("button");
-    button.classList.add("remove-button");
-    button.textContent = "[x]";
-    return button;
+const createRemoveButton = () => {
+    const removeButton = document.createElement("button");
+    removeButton.classList.add("remove-button");
+    removeButton.textContent = "[x]";
+    return removeButton;
 }
-const removeLi = (eventTarget) => {
-    const liWillRemoved = eventTarget.closest('li');
-    liWillRemoved.remove();
+const removeToDo = (eventTarget) => {
+    eventTarget.closest('li').remove();
 }
 const removeButton = (eventTarget) => {
     eventTarget.remove();
 }
 const pressButton = (eventTarget) =>{
-    const listOfButtons = filterForm.querySelectorAll('.filter_form-button')
-    listOfButtons.forEach((item) =>{
-        if(item == eventTarget){
+    listOfFilterButtons.forEach((item) =>{
+        if(item === eventTarget){
             item.classList.add('filter_form-button-pressed')
         }
         else{
             item.classList.remove('filter_form-button-pressed')
         }
     })
-    currentButton = eventTarget;
+    filterCurrentButton = eventTarget;
 }
-const showListOfLiIfCompleted = (arrayWithLiUncompleted, arrayWithLiCompleted) => {
-    arrayWithLiUncompleted.forEach((item) => {
-        item.classList.add('to_do_list-One-hidden');
-    })
-    arrayWithLiCompleted.forEach((item) => {
-        item.classList.remove('to_do_list-One-hidden');
-    })
+const showListOfCompleted = (arrayWithLiUncompleted, arrayWithLiCompleted) => {
+    arrayWithLiUncompleted.forEach(item => (item.classList.add('to_do_list-One-hidden')));
+    arrayWithLiCompleted.forEach(item => (item.classList.remove('to_do_list-One-hidden')));
 }
-const showListOfLiIfUnCompleted = (arrayWithLiCompleted, arrayWithLiUncompleted) => {
-    arrayWithLiCompleted.forEach((item) => {
-        item.classList.add('to_do_list-One-hidden');
-    })
-    arrayWithLiUncompleted.forEach((item) => {
-        item.classList.remove('to_do_list-One-hidden');
-    })
+const showListOfUncompleted = (arrayWithLiCompleted, arrayWithLiUncompleted) => {
+    arrayWithLiCompleted.forEach(item => (item.classList.add('to_do_list-One-hidden')));
+    arrayWithLiUncompleted.forEach(item => (item.classList.remove('to_do_list-One-hidden')));
 }
 const createListOfCompleted = () => {
-    const listOfLi = tableWithLi.querySelectorAll('.to_do_list-One-completed');
-    return listOfLi;
+    return arrayWithToDo.filter( item => (item.classList.contains('to_do_list-One-completed')));
 }
 const createListOfUncompleted = () => {
-    const arrayWithLi = tableWithLi.querySelectorAll('li');
-    const listOfLi = [].filter.call(arrayWithLi, (item) => {
-        return !(item.classList.contains('to_do_list-One-completed'));
-    })
-    return listOfLi;
+    return arrayWithToDo.filter( item => ( !(item.classList.contains('to_do_list-One-completed') )));
 }
 const showAllLi = () => {
-    const arrayWithLi = tableWithLi.querySelectorAll('li');
-    arrayWithLi.forEach((item) => {
-        item.classList.remove('to_do_list-One-hidden')
-    })
+    arrayWithToDo.forEach(item => (item.classList.remove('to_do_list-One-hidden')));
 }
-const chooseWhichListOfLiShow = () => {
-    if (currentButton == sortedByCompleted) {
-        showListOfLiIfCompleted(createListOfUncompleted(), createListOfCompleted());
+const chooseWhichListShow = () => {
+    if (filterCurrentButton == sortedByCompleted) {
+        showListOfCompleted(createListOfUncompleted(), createListOfCompleted());
     } else {
-        showListOfLiIfUnCompleted(createListOfCompleted(), createListOfUncompleted());
+        showListOfUncompleted(createListOfCompleted(), createListOfUncompleted());
     }
 }
-const filterByText = (textValueOfLi, filter, oneLi) => {
-    if (textValueOfLi.indexOf(filter) != -1) {
-        oneLi.classList.remove('to_do_list-One-hidden')
+const filterByText = (textValueOfToDo, filter, oneToDo) => {
+    if (textValueOfToDo.includes(filter)) {
+        oneToDo.classList.remove('to_do_list-One-hidden')
     } else {
-        oneLi.classList.add('to_do_list-One-hidden')
+        oneToDo.classList.add('to_do_list-One-hidden')
     }
 }
-const makeSearchWithFilter = (textValueOfLi, filter, oneLi) => {
-    switch (currentButton) {
+const makeSearchWithFilter = (textValueOfToDo, filter, oneToDo) => {
+    switch (filterCurrentButton) {
         case sortedByAll:
-            filterByText(textValueOfLi, filter, oneLi);
+            filterByText(textValueOfToDo, filter, oneToDo);
             break;
         case sortedByCompleted:
-            if (oneLi.classList.contains('to_do_list-One-completed')) {
-                filterByText(textValueOfLi, filter, oneLi);
+            if (oneToDo.classList.contains('to_do_list-One-completed')) {
+                filterByText(textValueOfToDo, filter, oneToDo);
             }
             break;
         case sortedByUncompleted:
-            if (!oneLi.classList.contains('to_do_list-One-completed')) {
-                filterByText(textValueOfLi, filter, oneLi);
+            if (!oneToDo.classList.contains('to_do_list-One-completed')) {
+                filterByText(textValueOfToDo, filter, oneToDo);
             }
+            break;
+        default:
             break;
     }
 }
-
-inputAdd.addEventListener("submit", (event) => {
+addToDoForm.addEventListener("submit", (event) => {
     event.preventDefault();
-    tableWithLi.append(createLi());
-    tableWithLi.lastChild.append(createButton());
-    inputWithText.value = '';
+    tableWithToDo.append(createToDo());
+    tableWithToDo.lastChild.append(createRemoveButton());
+    arrayWithToDo.push(tableWithToDo.lastChild);
+    textOfAddToDoForm.value = '';
 })
-tableWithLi.addEventListener('click', (event) => {
+tableWithToDo.addEventListener('click', (event) => {
     if (event.target.closest(".remove-button")) {
-        removeLi(event.target);
+        removeToDo(event.target);
         removeButton(event.target);
     }
 })
-tableWithLi.addEventListener('click', (event) => {
-    // Make Li completed or not;
+tableWithToDo.addEventListener('click', (event) => {
     event.target.classList.toggle('to_do_list-One-completed');
 })
 filterForm.addEventListener('click', (event) => {
     pressButton(event.target);
-    (currentButton == sortedByAll) ? showAllLi() : chooseWhichListOfLiShow();
+    (filterCurrentButton == sortedByAll) ? showAllLi() : chooseWhichListShow();
     searchFormInput.value = '';
 })
-searchForm.addEventListener('input', (event) => {
-    event.preventDefault();
-    const allListOfLi = tableWithLi.querySelectorAll('li');
+searchFormInput.addEventListener('input', (event) => {
     const filter = event.target.value.toLowerCase();
-    allListOfLi.forEach((oneLi) => {
-        const textValueOfLi = oneLi.textContent.toLowerCase() || oneLi.innerText.toLowerCase();
-        makeSearchWithFilter(textValueOfLi, filter, oneLi);
+    arrayWithToDo.forEach((oneToDo) => {
+        const textValueOfToDo = oneToDo.textContent.toLowerCase();
+        makeSearchWithFilter(textValueOfToDo, filter, oneToDo);
     })
 })
